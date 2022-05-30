0a16c066dc8d3b1449ba9844031c3979ff8cf302
Kevin Descamps <kevin.descamps2@soprasteria.com>
2022-05-30 17:29:40 +0200

    30 - 05 ------ Static

--- src/App.js
+++ src/App.js
@@ -2,64 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
+
+const StarDisplay = props => (
+    <>
+        {utils.range(1, props.counter).map(starId =>
+            <div key={starId} className="star"/>
+        )}
+    </>
+)
+
+
+const PlayNumber = props => (
+    <button className="number" onClick={() => console.log('Num', props.number)}>{props.number}</button>
+)
+
 function App() {
-  const [stars, setStars] = useState(utils.random(1,9));
-  return (
-      <div className="game">
-        <div className="help">
-          Pick 1 or more numbers that sum to the number of stars
+    const [stars, setStars] = useState(utils.random(1, 9));
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+            </div>
+            <div className="body">
+                <div className="left">
+                    < StarDisplay counter={stars}/>
+                </div>
+                <div className="right">
+                    {utils.range(1, 9).map(number =>
+                        <PlayNumber key={number} number={number}/>
+                    )}
+                </div>
+            </div>
+            <div className="timer">Time Remaining: 10</div>
         </div>
-        <div className="body">
-          <div className="left">
-            {utils.range(1,stars).map(starId =>
-            <div key={starId} className="star"/> )}
-          </div>
-          <div className="right">
-            {utils.range(1,9).map(number =>
-            <button key={number} className="number">{number} </button>)}
-          </div>
-        </div>
-        <div className="timer">Time Remaining: 10</div>
-      </div>
-  );
+    );
 };
 
 // Color Theme
 const colors = {
-  available: 'lightgray',
-  used: 'lightgreen',
-  wrong: 'lightcoral',
-  candidate: 'deepskyblue',
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
 };
 
 // Math science
 const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+    // Sum an array
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
 
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+    // create an array of numbers between min and max (edges included)
+    range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
 
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+    // pick a random number between min and max (edges included)
+    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
 
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr, max) => {
+        const sets = [[]];
+        const sums = [];
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0, len = sets.length; j < len; j++) {
+                const candidateSet = sets[j].concat(arr[i]);
+                const candidateSum = utils.sum(candidateSet);
+                if (candidateSum <= max) {
+                    sets.push(candidateSet);
+                    sums.push(candidateSum);
+                }
+            }
         }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
+        return sums[utils.random(0, sums.length - 1)];
+    },
 };
 
 export default App;
